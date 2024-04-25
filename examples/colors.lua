2a81cc4773e3d25aebc4f673b180a0be6bec5d6d
vlev <matiasvlevi@gmail.com>
2024-04-25 00:49:50 -0400

    color example

---
+++ examples/colors.lua
@@ -0,0 +1,16 @@
+
+function setup()
+    createWindow(400, 400); 
+
+    size = width/#colors;
+end
+
+function draw()
+    background(0);
+
+    for i=1, #colors do
+        fill(colors[i]);
+        rect(i*size - size, 0, size, height);
+    end
+end
+
