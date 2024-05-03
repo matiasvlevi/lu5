bbe8aca289fa340bc896282111dd55763e757362
vlev <matiasvlevi@gmail.com>
2024-05-03 00:59:46 -0400

    builds

--- examples/text.lua
+++ examples/text.lua
@@ -1,23 +1,29 @@
-counter = 0;
+x = 0;
 
 function setup()
-    createWindow(400, 400); 
+    createWindow(600, 600); 
+
+    loadFont("/usr/share/fonts/TTF/Hack-Regular.ttf");
 end
 
 function draw()
     background(51);
 
     fill(255, 150, 50);
-    textSize(32);
+    textSize(48);
     text('Text demo', 30, 10);
     
     fill(190, 255, 40);
-    textSize(18);
-    text('Position: ' .. round(counter), 30, 40);
+    textSize(32);
+    text('Position: ' .. x, 30, 40);
 
     fill(67, 165, 210);
-    square(counter, 200, 32);
+    square(x, 200, 32);
 
-    counter = (counter + 4) % width;
+    if (x > width) then
+        x = 0
+    else
+        x = x + 4;
+    end
 end
 
