178159d206a72fee6892d90a94548d8791a76682
vlev <matiasvlevi@gmail.com>
2024-05-03 20:19:54 -0400

    text example

--- examples/text.lua
+++ examples/text.lua
@@ -1,29 +1,42 @@
-x = 0;
 
 function setup()
     createWindow(600, 600); 
 
-    loadFont("/usr/share/fonts/TTF/Hack-Regular.ttf");
+    -- Replace these with your font paths
+    font_arial = loadFont("/usr/share/fonts/TTF/Arial.TTF");
+    font_times = loadFont("/usr/share/fonts/TTF/Times.TTF");
+    font_georgia = loadFont("/usr/share/fonts/TTF/Georgia.TTF");
 end
 
 function draw()
     background(51);
 
+    textFont(font_arial);
     fill(255, 150, 50);
-    textSize(48);
-    text('Text demo', 30, 10);
-    
-    fill(190, 255, 40);
+
     textSize(32);
-    text('Position: ' .. x, 30, 40);
+    text('This is Arial', 30, 10);
 
-    fill(67, 165, 210);
-    square(x, 200, 32);
+    textSize(16);
+    text('Same font but in tiny', 30, 30);
+    
+    textFont(font_times);
+    fill(190, 255, 40);
 
-    if (x > width) then
-        x = 0
-    else
-        x = x + 4;
-    end
+    textSize(32);
+    text('This is Times New Roman', 30, 50);
+
+    textSize(22);
+    text('And this is some smaller text', 30, 70);
+
+    textFont(font_georgia);
+    fill(255, 40, 190);
+
+    textSize(32);
+    text('This is Georgia', 30, 100);
+
+    textSize(22);
+    text('Same as above but smaller', 30, 120);
+
 end
 
