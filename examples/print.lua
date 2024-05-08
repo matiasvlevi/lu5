531ad6048f56c94d419176f5086c29bb0597a67d
vlev <matiasvlevi@gmail.com>
2024-05-08 00:48:20 -0400

    cleaner print example

--- examples/print.lua
+++ examples/print.lua
@@ -1,26 +1,17 @@
+print(1);
+print(1, 2);
+print(1, 2, 3);
+print(1, 2, 3, 4);
 
--- Strings
---print('Hello from lua');
+print(1.);
+print(1., 2.);
+print(1., 2., 3.);
+print(1., 2., 3., 4.);
 
--- Lists
---print({'weqweewe', 2, 3}, 2);
+print({ 1, 2, 3 });
+print({ 1, 2, 3 }, { 2, 3, 4 });
+print({ 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 });
 
-
-print(1)
-print(1, 2)
-print(1, 2, 3)
-print(1, 2, 3, 4)
-
-
-print('Hello')
-print('Hello', 2)
-print('Hello', 2, 3)
-print('Hello', 2, true, { a=4 })
-
-print('Hello')
-print('Hello', {2, 2, 3, false, 3.322212323,3, 3,3})
-print('Hello', 2, 3)
-print('Hello', 2, 3, 4)
-print({ 'text', { 1, 2, { 6, {2, 'hello', 7, {8, 9, 1, 3}, 2, 3}, 'this is a string', 2 } }, 2, 3 });
-print({ {1, 2}, 1, { 1, 2, { 6, 2,  7, {5, 6, 3, {2, 3, 3}, 1}, 2 } }, 2, 3 });
-
+print('Hello');
+print('Hello', 'Hello');
+print('Hello', 'Hello', 'Hello');
