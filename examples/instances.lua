2c8ea9f53f03ef7ea07887fd51af0e9ef4453401
vlev <matiasvlevi@gmail.com>
2024-05-11 17:17:17 -0400

    logo instances

--- examples/instances.lua
+++ examples/instances.lua
@@ -1,120 +1,78 @@
 
-acc = 1;
-bounce = 1.5;
-ball_count = 300;
+size = 40;
+logo_count = 100;
 
-mouse_zone = 256
+mouse_zone = 300
 
--- Ball list
-balls = {}
+-- Logos list
+logos = {}
 
--- Ball class
-local Ball = class('Ball');
+-- Logo class
+local Logo = class('Logo');
 
-function Ball:init(x, y)
+function Logo:init(x, y, vx, vy)
     self.x = x
     self.y = y
-    self.vx = 0;
-    self.vy = 0;
-    self.r = 16;
+    self.vx = vx;
+    self.vy = vy;
+    self.size = size;
 
-    self.cr = math.random(155, 255);
-    self.cg = math.random(0, 155);
-    self.cb = math.random(155, 255);
-
-    return self
+    return self;
 end
 
-function Ball:collision(others)
+function Logo:collision(others)
     local nx = self.x + self.vx * deltaTime;
     local ny = self.y + self.vy * deltaTime;
 
-    if (ny < self.r or ny > height-self.r) then 
+    if (ny < 0 or ny > height-self.size) then 
         self.vy = -self.vy; 
     end
 
-    if (nx < self.r or nx > width-self.r) then
+    if (nx < 0 or nx > width-self.size) then
         self.vx = -self.vx;
     end
-
-    for i, other in pairs(others) do
-        local dx = self.x - other.x;
-        local dy = self.y - other.y;
-        local d = math.sqrt(dx*dx + dy*dy);
-
-        if (d < 16) then
-            self.vx = self.vx + dx * bounce;
-            self.vy = self.vy + dy * bounce;
-        end
-    end
-
-    local dx = nx - mouseX;
-    local dy = ny - mouseY;
-    local d = math.sqrt(dx*dx + dy*dy);
-    if (d < 1.5+8+mouse_zone/2) then
-
-        if (mouseIsPressed) then
-            self.vx = self.vx + -dx * bounce/5;
-            self.vy = self.vy + -dy * bounce/5;
-        else
-            self.vx = self.vx + dx * bounce/2;
-            self.vy = self.vy + dy * bounce/2;
-        end
-
-
-    end
-
-
 end
 
-function Ball:move()
-    self.vx = self.vx + math.random(-acc, acc);
-    self.vy = self.vy + math.random(-acc, acc);
-
-    self.vy = self.vy * 0.94;
-    self.vx = self.vx * 0.94;
-
+function Logo:move()
     self:collision(balls);
 
     self.x = self.x + self.vx * deltaTime;
     self.y = self.y + self.vy * deltaTime;
-end 
-
-function Ball:draw()
-    strokeWeight(3);
-    stroke(self.cr, self.cg, self.cb);
-    fill(255, 255, 255, 25)
-    circle(self.x, self.y, 16);
 end
 
-
+function Logo:draw()
+    fill(255, 150);
+    image(img, self.x, self.y, self.size, self.size);
+end
 
 function setup() 
     createWindow(800, 600); 
 
-    for i=1, ball_count do
-      balls[i] = Ball:new(
+    for i=1, logo_count do
+      logos[i] = Logo:new(
             math.random(50, width-50),
-            math.random(50, height-50)
+            math.random(50, height-50),
+            math.random(-100, 100),
+            math.random(-100, 100)
         );
     end
 
-    frameRate(60);
+    img = loadImage('./docs/assets/logo.png');
 
-    logo = loadImage('docs/assets/lu5_icon.png');
+    frameRate(60);
 end
 
 function draw()
     background(51);
 
-    for i,ball in pairs(balls) do
-        ball:draw();
-        ball:move();
+    for i,logo in pairs(logos) do
+        logo:draw();
+        logo:move();
     end
 
-    circle(mouseX, mouseY, mouse_zone);
-
+    push();
     textSize(18);
     fill(100, 255, 10);
     text(round(frameRate()) .. ' fps', 30, 10);
+    pop();
 end
