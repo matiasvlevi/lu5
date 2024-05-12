eec0de5e64561ab241b30d5ba48b68053b6df2ac
vlev <matiasvlevi@gmail.com>
2024-05-11 22:53:42 -0400

    keyboard globals

--- src/bindings/keyboard.h
+++ src/bindings/keyboard.h
@@ -6,7 +6,19 @@
 /**
  * Check if a keyboard key is pressed, returns `true` if key is down and returns `false` if it's not
  * 
- * @param k The keyboard key to check
+ * @param k The keyboard key to check as a key global, or a string
+ *
+ * @example
+ * -- Use a string 
+ * if (isKeyDown('a')) then
+ *   -- key 'a' is down
+ * end
+ *
+ * -- use a global
+ * if (isKeyDown(LEFT_ARROW)) then
+ *   -- left arrow is down
+ * end
+ * @example
  *
  * @return boolean Whether or not the key is down
  */
@@ -19,7 +31,8 @@ int isKeyDown(lua_State *L);
  *
  * @param key The pressed key
  *
- */ 
+ */
+#define LU5_KEY_PRESSED "keyPressed"
 
 /**
  * @brief keyReleased
@@ -28,6 +41,7 @@ int isKeyDown(lua_State *L);
  *
  * @param key The released key
  */ 
+#define LU5_KEY_RELEASED "keyReleased"
 
 /**
  * @brief keyHeld
@@ -36,5 +50,48 @@ int isKeyDown(lua_State *L);
  *
  * @param key The held key
  */ 
+#define LU5_KEY_HELD "keyHeld"
+
+/**
+ * @brief UP_ARROW
+ * @global
+ * 
+ */
+#define LU5_KEY_UP_ARROW 265
+
+/**
+ * @brief DOWN_ARROW
+ * @global
+ * 
+ */
+#define LU5_KEY_DOWN_ARROW 264
+
+/**
+ * @brief LEFT_ARROW
+ * @global
+ * 
+ */
+#define LU5_KEY_LEFT_ARROW 263
+
+/**
+ * @brief RIGHT_ARROW
+ * @global
+ * 
+ */
+#define LU5_KEY_RIGHT_ARROW 262
+
+/**
+ * @brief ENTER
+ * @global
+ * 
+ */
+#define LU5_KEY_ENTER 257
+
+/**
+ * @brief BACKSPACE
+ * @global
+ * 
+ */
+#define LU5_KEY_BACKSPACE 259
 
 #endif
