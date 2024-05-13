90d0c5b8dddd5708ee62a2fb460e0e949b5c55c0
SharkAce <charles.marchildon2@gmail.com>
2024-05-13 18:01:49 -0400

    Added math bindings and a math.lua example

---
+++ examples/math.lua
@@ -0,0 +1,58 @@
+
+function slider(x1,x2,y,value)
+	push();
+	strokeWeight(4);
+	fill(255,255,255);
+
+	line(x1,y,x2,y);
+	circle(map(value,-100,100,x1,x2),y,15);
+	pop();
+end
+
+function setup()
+   createWindow(600, 600);
+end
+
+function draw()
+  background(51);
+	textSize(25);
+
+	sliderValue = map(constrain(mouseX,50,250),50,250,-100,100);
+	text("value: " .. sliderValue, 50, 50);
+	slider(50,250,100,sliderValue);
+
+	text("abs function:", 50, 150);
+	slider(50,250,200,abs(sliderValue));
+
+	text("map function:", 50, 250);
+	slider(50,250,300,map(sliderValue,-100,100,-30,30));
+	push();
+	fill(255,0,0);
+	circle(map(-40,-100,100,50,250),300,10);
+	circle(map(40,-100,100,50,250),300,10);
+	pop();
+
+	text("constrain function:", 50, 350);
+	slider(50,250,400,constrain(sliderValue,-60,60));
+	push();
+	fill(255,0,0);
+	circle(map(-70,-100,100,50,250),400,10);
+	circle(map(70,-100,100,50,250),400,10);
+	pop();
+
+	text("dist function:", 350, 50);
+	x1 = 450;
+	y1 = 250;
+	x2 = constrain(mouseX,350,550);
+	y2 = constrain(mouseY,150,350);
+	text("distance: " .. floor(dist(x1,y1,x2,y2)), 350, 100);
+	push();
+	strokeWeight(4);
+	fill(255,255,255);
+	line(x1,y1,x2,y2);
+	circle(x1,y1,15);
+	circle(x2,y2,15);
+	pop();
+
+
+end
--- src/bindings/lu5_math.c
+++ src/bindings/lu5_math.c
@@ -32,6 +32,56 @@ int lu5_ceil(lua_State *L)
 	return 1;
 }
 
+int lu5_abs(lua_State *L)
+{
+	double x = lua_tonumber(L, 1);
+
+	if (x < 0) x *= -1;
+	
+	lua_pushnumber(L, x);
+	return 1;
+}
+
+int lu5_map(lua_State *L)
+{
+	double x = lua_tonumber(L, 1);
+	double s1 = lua_tonumber(L, 2);
+	double e1 = lua_tonumber(L, 3);
+	double s2 = lua_tonumber(L, 4);
+	double e2 = lua_tonumber(L, 5);
+
+	double ratio = (x - s1) / (e1 - s1);
+	
+	lua_pushnumber(L, ((e2 - s2) * ratio) + s2);
+	return 1;
+}
+
+int lu5_dist(lua_State *L)
+{
+	double X1 = lua_tonumber(L, 1);
+	double Y1 = lua_tonumber(L, 2);
+	double X2 = lua_tonumber(L, 3);
+	double Y2 = lua_tonumber(L, 4);
+
+	double value = sqrt(pow(X2 - X1, 2) + pow(Y2 - Y1, 2));
+	
+	lua_pushnumber(L, value);
+	return 1;
+}
+
+int lu5_constrain(lua_State *L)
+{
+	double x = lua_tonumber(L, 1);
+	double min = lua_tonumber(L, 2);
+	double max = lua_tonumber(L, 3);
+
+	if (x < min) x = min;
+	if (x > max) x = max;
+	
+	lua_pushnumber(L, x);
+	return 1;
+}
+
 int lu5_sin(lua_State *L)
 {
 	double value = lua_tonumber(L, 1);
