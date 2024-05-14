3443e6632f43f748fee3bae6388df068e659726c
vlev <matiasvlevi@gmail.com>
2024-05-13 21:51:29 -0400

    tweak text example

--- examples/text.lua
+++ examples/text.lua
@@ -18,25 +18,24 @@ function draw()
     text('This is Arial', 30, 10);
 
     textSize(16);
-    text('Same font but in tiny', 30, 30);
-    
+    text('Same font but in tiny', 30, 42);
+
     textFont(font_times);
     fill(190, 255, 40);
 
     textSize(32);
-    text('This is Times New Roman', 30, 50);
+    text('This is Times New Roman', 30, 80);
 
     textSize(22);
-    text('And this is some smaller text', 30, 70);
+    text('And this is some smaller text', 30, 112);
 
     textFont(font_hack);
     fill(255, 40, 190);
 
     textSize(32);
-    text('This is the Hack font', 30, 100);
+    text('This is the Hack font', 30, 150);
 
     textSize(22);
-    text('Same as above but smaller', 30, 120);
-
+    text('Same as above but smaller', 30, 192);
 end
 
