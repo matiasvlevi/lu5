ba3253e5203e055b3526f198f9b2bfec9ca03024
vlev <matiasvlevi@gmail.com>
2024-05-22 03:40:01 -0400

    better error messages for math bindings

--- src/bindings/lu5_math.c
+++ src/bindings/lu5_math.c
@@ -7,6 +7,110 @@
 #include <math.h>
 #include <float.h>
 
+static double lu5_assert_number(lua_State *L, int index, const char *fname) {
+	if (lua_isnumber(L, index)) 
+		return lua_tonumber(L, index);
+
+	int type = lua_type(L, index);
+	const char* typename = lua_typename(L, type);
+
+	return
+		luaL_error(L, "Function \x1b[90m'%s'\x1b[0m expected \x1b[90m'number'\x1b[0m at argument \x1b[33m%d\x1b[0m. Found %s instead.", fname, index, typename);
+}
+
+int lu5_round(lua_State *L) 
+{
+	double value = lu5_assert_number(L, 1, "round");
+
+	lua_pushinteger(L, round(value));
+	return 1;
+}
+
+int lu5_floor(lua_State *L)
+{
+	double value = lu5_assert_number(L, 1, "floor");
+
+	lua_pushinteger(L, floor(value));
+	return 1;
+}
+
+int lu5_ceil(lua_State *L)
+{
+	double value = lu5_assert_number(L, 1, "ceil");
+
+	lua_pushinteger(L, ceil(value));
+	return 1;
+}
+
+int lu5_abs(lua_State *L)
+{
+	double x = lu5_assert_number(L, 1, "abs");
+
+	lua_pushnumber(L, fabs(x));
+	return 1;
+}
+
+int lu5_map(lua_State *L)
+{
+	double x  = lu5_assert_number(L, 1, "map");
+	double s1 = lu5_assert_number(L, 2, "map");
+	double e1 = lu5_assert_number(L, 3, "map");
+	double s2 = lu5_assert_number(L, 4, "map");
+	double e2 = lu5_assert_number(L, 5, "map");
+
+	double ratio = (x - s1) / (e1 - s1);
+	
+	lua_pushnumber(L, ((e2 - s2) * ratio) + s2);
+	return 1;
+}
+
+int lu5_dist(lua_State *L)
+{
+	double x1 = lu5_assert_number(L, 1, "dist");
+	double y1 = lu5_assert_number(L, 2, "dist");
+	double x2 = lu5_assert_number(L, 3, "dist");
+	double y2 = lu5_assert_number(L, 4, "dist");
+
+	double value = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	
+	lua_pushnumber(L, value);
+	return 1;
+}
+
+int lu5_constrain(lua_State *L)
+{
+	double x   = lu5_assert_number(L, 1, "constrain");
+	double min = lu5_assert_number(L, 2, "constrain");
+	double max = lu5_assert_number(L, 3, "constrain");
+	
+	lua_pushnumber(L, fmax(fmin(x, max), min));
+	return 1;
+}
+
+int lu5_sin(lua_State *L)
+{
+	double value = lu5_assert_number(L, 1, "sin");
+
+	lua_pushnumber(L, sin(value));
+	return 1;
+}
+
+int lu5_cos(lua_State *L)
+{
+	double value = lu5_assert_number(L, 1, "cos");
+
+	lua_pushnumber(L, cos(value));
+	return 1;
+}
+
+int lu5_tan(lua_State *L)
+{
+	double value = lu5_assert_number(L, 1, "tan");
+
+	lua_pushnumber(L, tan(value));
+	return 1;
+}
+
 int lu5_randomSeed(lua_State *L) 
 {
 	LU5_ONE_TO_ONE_BINDING("math", "randomseed");
@@ -17,50 +121,52 @@ int lu5_random(lua_State *L)
 {
 	int argc = lua_gettop(L);
 
-	// Call random
+	// Push a random value on the stack
 	lua_getglobal(L, "math");
 	lua_getfield(L, -1, "random");
 	lua_call(L, 0, 1);
 
-	// Branch according to number of args
 	switch(argc) {
-		// Return random value as is if 0 arguments found
+		// Return random value as is
 		case 0: return 1;
 
+		// Handle one argument
 		case 1: {
+			// Get the random value
 			double value = lua_tonumber(L, -1);
-			// Handle scalar range
-			if (lua_isnumber(L, 1)) 
-			{
-				double scalar = lua_tonumber(L, 1);
-				lua_pushnumber(L, scalar * value);
-				return 1;
-			}
-			// Handle Table
-			else if (lua_istable(L, 1)) 
+
+			// Handle table
+			if (lua_istable(L, 1)) 
 			{
 				int len = lua_rawlen(L, 1);
-				int index = ceil(len * value);
 
-				lua_pushinteger(L, index);
+				// Map random value to index
+				lua_pushinteger(L, (int)ceil(len * value));
+				
+				// Return element at index
 				lua_gettable(L, 1);
+				return 1;
+			}			
+			// Handle number as scalar range
+			else
+			{
+				// Get arguments
+				double scalar = lu5_assert_number(L, 1, "random");
 
+				// Scale random value
+				lua_pushnumber(L, scalar * value);
 				return 1;
 			}
 		}
 
 		// Handle min/max range
 		case 2: {
+			// Get random value
 			double value = lua_tonumber(L, -1);
 
-			double min, max;
-			if (lua_isnumber(L, 1) && lua_isnumber(L, 2)) {
-				min = lua_tonumber(L, 1);
-				max = lua_tonumber(L, 2);
-			} else {
-				luaL_error(L, "Expected number as arguments");
-				return 0;
-			}
+			// Get arguments
+			double min = lu5_assert_number(L, 1, "random");
+			double max = lu5_assert_number(L, 2, "random");
 
 			// Map in range
 			lua_pushnumber(L, value * (max - min) + min);
@@ -68,45 +174,10 @@ int lu5_random(lua_State *L)
 		}
 
 		default: {
-			luaL_error(L, "Expected 0 to 2 arguments, got %i", argc);
+			luaL_error(L, "random expected 0 to 2 arguments, got %i", argc);
 			return 0;
 		}
 	}
-}
-
-int lu5_round(lua_State *L) 
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushinteger(L, round(value));
-	return 1;
-}
-
-int lu5_floor(lua_State *L)
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushinteger(L, floor(value));
-	return 1;
-}
-
-int lu5_ceil(lua_State *L)
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushinteger(L, ceil(value));
-	return 1;
-}
-
-static double lu5_assert_number(lua_State *L, int index, const char *fname) {
-	if (lua_isnumber(L, index)) 
-		return lua_tonumber(L, index);
-
-	int type = lua_type(L, index);
-	const char* typename = lua_typename(L, type);
-
-	return
-		luaL_error(L,  "Expected 'number' in function '%s' at argument %d. Found %s instead.", fname, index, typename);
 }
 
 int lu5_min(lua_State *L)
@@ -174,74 +245,5 @@ int lu5_max(lua_State *L)
 	}
 
 	lua_pushnumber(L, result);
-	return 1;
-}
-
-int lu5_abs(lua_State *L)
-{
-	double x = lua_tonumber(L, 1);
-
-	lua_pushnumber(L, fabs(x));
-	return 1;
-}
-
-int lu5_map(lua_State *L)
-{
-	double x = lua_tonumber(L, 1);
-	double s1 = lua_tonumber(L, 2);
-	double e1 = lua_tonumber(L, 3);
-	double s2 = lua_tonumber(L, 4);
-	double e2 = lua_tonumber(L, 5);
-
-	double ratio = (x - s1) / (e1 - s1);
-	
-	lua_pushnumber(L, ((e2 - s2) * ratio) + s2);
-	return 1;
-}
-
-int lu5_dist(lua_State *L)
-{
-	double x1 = lua_tonumber(L, 1);
-	double y1 = lua_tonumber(L, 2);
-	double x2 = lua_tonumber(L, 3);
-	double y2 = lua_tonumber(L, 4);
-
-	double value = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-	
-	lua_pushnumber(L, value);
-	return 1;
-}
-
-int lu5_constrain(lua_State *L)
-{
-	double x = lua_tonumber(L, 1);
-	double min = lua_tonumber(L, 2);
-	double max = lua_tonumber(L, 3);
-	
-	lua_pushnumber(L, fmax(fmin(x, max), min));
-	return 1;
-}
-
-int lu5_sin(lua_State *L)
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushnumber(L, sin(value));
-	return 1;
-}
-
-int lu5_cos(lua_State *L)
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushnumber(L, cos(value));
-	return 1;
-}
-
-int lu5_tan(lua_State *L)
-{
-	double value = lua_tonumber(L, 1);
-
-	lua_pushnumber(L, tan(value));
 	return 1;
 }
