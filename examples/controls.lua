6289781d29b58e5a7c13a1cf5b9c2fd785bd42c6
vlev <matiasvlevi@gmail.com>
2024-06-11 21:30:59 -0400

    controls example

---
+++ examples/controls.lua
@@ -0,0 +1,38 @@
+x = 300;
+y = 300;
+vx = 0;
+vy = 0;
+
+
+function setup()
+	createWindow(600, 600);
+end
+
+function draw()
+	background(51);
+
+	if (keyIsDown('a')) then
+		vx = vx - 10;
+	end
+	if (keyIsDown('d')) then
+		vx = vx + 10;
+	end
+	if (keyIsDown('w')) then
+		vy = vy - 10;
+	end
+	if (keyIsDown('s')) then
+		vy = vy + 10;
+	end
+
+	fill(255);
+	strokeWeight(6);
+	circle(x, y, 32);
+
+	line(x,y,x+vx/8,y+vy/8);
+
+	x = x + vx * deltaTime;
+	y = y + vy * deltaTime;
+
+	vx = vx * 0.99;
+	vy = vy * 0.99;
+end
