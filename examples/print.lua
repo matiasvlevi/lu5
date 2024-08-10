49dcc4bfbdceea1f3bebd8473f8de6b78ff6cf7f
vlev <matiasvlevi@gmail.com>
2024-08-10 03:05:56 -0400

    better print example

--- examples/print.lua
+++ examples/print.lua
@@ -30,5 +30,31 @@ print('More tables:')
 print({ 'Table', 'of', 'strings' });
 print({ 'Table', 'of', 'more', 'strings' });
 
+
+print();
+
+local v = createVector(4, 5);
+print('Vector:', v);
+
+print()
+print('Classes & instances')
+
+local Ball = class('Ball');
+
+function Ball:init(x, y)
+    self.x = x;
+    self.y = y;
+    
+    return self;
+end
+
+
+print(Ball)
+print(Ball:new(260, 100))
+print(Ball:new(200, 500))
+print(Ball:new(275, 120))
+print(Ball:new(100, 350))
+
+print();
 print('--- LU5 PRINT EXAMPLE END ---');
 print();
---
+++ examples/vector.lua
@@ -0,0 +1,11 @@
+
+local a = createVector(3, 4);
+local b = createVector(5, 6);
+
+a:print();
+b:print();
+
+local c = a:mult(b);
+
+c:print();
+a:print();
