efb0f40e89b592e0d09d8cf066b39247e5a6d2d1
vlev <matiasvlevi@gmail.com>
2024-09-01 04:15:59 -0400

    model docs

--- src/bindings/models.h
+++ src/bindings/models.h
@@ -4,19 +4,33 @@
 #include <lua.h>
 
 /**
- * Draw a 3D model
+ * Load a 3D model
+ * 
+ * @param model The 3D model
+ *
+ * @example
+ *
+ * function setup()
+ *   createWindow(600, 600, GL3D);
+ *   
+ *   ship = loadModel('path/to/your/ship.obj');
+ * end
+ *
+ * function draw()
+ *   background(51);
+ *
+ *   model(ship);
+ * end
+ * @example
+ */
+int loadModel(lua_State *L);
+
+/**
+ * Render a 3D model
  * 
  * @param model The 3D model
  *
  */
 int model(lua_State *L);
-
-/**
- * Load a 3D model
- * 
- * @param model The 3D model
- *
- */
-int loadModel(lua_State *L);
 
 #endif /* _LU5_MODELS_BINDINGS_ */
